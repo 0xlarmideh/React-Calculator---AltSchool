c47c356f0374e37c2d049624ecbdc2bf25997713
Abdulhameed Busari <65597134+0xlarmideh@users.noreply.github.com>
2022-10-11 02:28:59 +0000

    Fixed the grid layout.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ export default function Calculator() {
   const calcInput = useRef(null);
   const [result, setResult] = useState();
   // Set focus to the calculator input
-   useEffect(() => {
-     // setResult(0);
-     // calcInput.current.focus();
-  }, [])
+ //  useEffect(() => {
+ //    setResult(0);
+ //    calcInput.current.focus();
+ // }, [])
 
   function addToInput(e) {
     calcInput.current.focus();
@@ -17,19 +17,31 @@ export default function Calculator() {
     e.preventDefault();
   }
 
-  // Error boundaries
-  
   // Function to do the calculations once the equate button is pressed
   function handleClick(event) {
-    const elements = calcInput.current.value
-    const x = eval(`${elements}`)
+    let elements = calcInput.current.value;
+    const elementsSplit = elements.split("");
+    if(elementsSplit[0] == 0){
+      elementsSplit.shift();
+      elements = elementsSplit.join("")
+    }
+    let x = eval(`${elements}`);
+    const y = String (x).split("");
 
     // Check if input value is empty
-    if (calcInput.current.value === "") {
+    if (elements === "") {
       setResult(0)
     } else {
       setResult(x);
-      calcInput.current.value = x
+    }
+    if(elementsSplit[0] == 0){
+      elementsSplit.shift();
+      elements = elementsSplit.join("")
+    }
+    if(y.includes(".")){
+      x = parseFloat(x).toFixed(4);
+      setResult(x);
+      console.log("decimals")
     }
     calcInput.current.focus();
     event.preventDefault();
@@ -39,54 +51,38 @@ export default function Calculator() {
     const inputValue = calcInput.current.value
     let x = inputValue.split("")
     x.pop()
-    calcInput.current.value = x.join("")
+    calcInput.current.value = x.join("");
+    
   }
 
   return (
-    <div className="calculator">
+    <div className="container">
       <div className="calc-items">
-        <form >
-          <div className="text-box">
-            <input className='input-text' name="values" ref={calcInput} onFocus={(e) => e.target.readOnly = true}></input>
-            <div className='result'>{result}</div>
-          </div>
-
-          <div className="cols-4">
-            <input className="items grey" value="AC" type="reset" onClick={() => setResult()} />
-            <input className='items green' type="button" value="x" onClick={deleteValue} />
-            <button className='items green' onClick={addToInput}>%</button>
-            <button className='items green margin-null' onClick={addToInput}>/</button>
-          </div>
-          <div className="cols-4">
+            <div className="text-box output">
+              <input className='input-text' name="values" ref={calcInput} onFocus={(e) => e.target.readOnly = true}></input>
+              <div className='result'>{result}</div>
+            </div>
+            <input className="items clear" value="AC" type="reset" onClick={() => {
+        setResult();
+      calcInput.current.value =""}
+            }></input>
+            <button className='items clear span-2' onClick={deleteValue}>DEL</button>             <button className='items operands ' onClick={addToInput}>/</button>
             <button className='items' onClick={addToInput}>7</button>
             <button className='items' onClick={addToInput}>8</button>
             <button className='items' onClick={addToInput}>9</button>
-            <button className='items green margin-null' onClick={addToInput}>*</button>
-          </div>
-
-          <div className="cols-4">
+            <button className='items operands' onClick={addToInput}>*</button>
             <button className='items' onClick={addToInput}>4</button>
             <button className='items' onClick={addToInput}>5</button>
             <button className='items' onClick={addToInput}>6</button>
-            <button className='items green margin-null' onClick={addToInput}>-</button>
-          </div>
-
-          <div className="cols-4">
+            <button className='items operands' onClick={addToInput}>-</button>
             <button className='items' onClick={addToInput}>1</button>
             <button className='items' onClick={addToInput}>2</button>
             <button className='items' onClick={addToInput}>3</button>
-            <button className='items green margin-null' onClick={addToInput}>+</button>
-          </div>
-
-          <div className="cols-4x">
+            <button className='items operands' onClick={addToInput}>+</button>
             <button className='items' onClick={addToInput}>0</button>
             <button className='items' onClick={addToInput}>.</button>
-            <button className='items cyan margin-null span-2' type="submit" onClick={handleClick}>=</button>
-          </div>
-        </form>
-
+            <button className='items equate  span-2' onClick={handleClick}>=</button>
+        </div>
       </div>
-    </div>
-
   );
 }
