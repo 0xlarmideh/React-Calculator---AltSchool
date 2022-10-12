9d9c9b305f50e70012506d2694fe540497cbaaab
Abdulhameed Busari <65597134+0xlarmideh@users.noreply.github.com>
2022-10-12 21:32:06 +0000

    Latest. Tried covering all edgecases. Code is messy, refactoring later.

--- src/App.jsx
+++ src/App.jsx
@@ -10,56 +10,73 @@ export default function Calculator() {
  //    calcInput.current.focus();
  // }, [])
 
+  // Fuction to add expression
   function addToInput(e) {
-    calcInput.current.focus();
     const text = e.target.innerHTML
-    calcInput.current.value = calcInput.current.value + text
+    calcInput.current.value += text;
+    let elements = calcInput.current.value;
+    let eleIndex = elements[elements.length-1];
+    let eleIndex2 = elements[elements.length-2];
+    const elementsSplit = elements.split("");
+
+    // Prevent division or multiplication starting the expression
+    if(elementsSplit[0] == 0 || elementsSplit[0] == "/" || elementsSplit[0] == '*'){
+      elementsSplit.shift();
+      elements = elementsSplit.join("")
+      calcInput.current.value = elements;
+    }
+    // Prevent two signs from being input. (Very bad implementation, will refactor later)
+      // Already found a better way to implement this.
+    else if (eleIndex == '/' && eleIndex2 == '/' || eleIndex == '*' && eleIndex2 == '*' || eleIndex == '-' && eleIndex2 == '-' || eleIndex == '-' && eleIndex2 == '+' || eleIndex == '*' && eleIndex2 == '-' || eleIndex == '/' && eleIndex2 == '*' || eleIndex == '*' && eleIndex2 == '/' || eleIndex == '+' && eleIndex2 == '-' || eleIndex == '/' && eleIndex2 == '+' || eleIndex == '/' && eleIndex2 == '-' || eleIndex == '*' && eleIndex2 == '+' || eleIndex == '+' && eleIndex2 == '+'){
+      elements = elements.slice(0, -1);
+      calcInput.current.value = elements;
+    }
     e.preventDefault();
   }
 
   // Function to do the calculations once the equate button is pressed
   function handleClick(event) {
     let elements = calcInput.current.value;
-    const elementsSplit = elements.split("");
-    if(elementsSplit[0] == 0){
-      elementsSplit.shift();
-      elements = elementsSplit.join("")
+    let eleIndex = elements[elements.length-1];
+
+    // Prevent operands from being the last element of the input
+    if(eleIndex == '/' || eleIndex == '*' || eleIndex == '+' || eleIndex == '-' ){
+      elements = elements.slice(0, -1);
+      calcInput.current.value = elements;
     }
     let x = eval(`${elements}`);
     const y = String (x).split("");
 
-    // Check if input value is empty
+    // Check if input value is empty and set state
     if (elements === "") {
       setResult(0)
     } else {
       setResult(x);
     }
-    if(elementsSplit[0] == 0){
-      elementsSplit.shift();
-      elements = elementsSplit.join("")
-    }
+
+    // Restrict decimal to 3 places
     if(y.includes(".")){
-      x = parseFloat(x).toFixed(4);
+      x = parseFloat(x).toFixed(3);
       setResult(x);
       console.log("decimals")
     }
-    calcInput.current.focus();
+    // calcInput.current.focus();
     event.preventDefault();
   }
 
+  // Delete a value
   const deleteValue = () => {
     const inputValue = calcInput.current.value
-    let x = inputValue.split("")
-    x.pop()
-    calcInput.current.value = x.join("");
-    
+    let x = inputValue.split("");
+    x.pop();
+    calcInput.current.value = x.join(""); 
   }
 
   return (
     <div className="container">
       <div className="calc-items">
             <div className="text-box output">
-              <input className='input-text' name="values" ref={calcInput} onFocus={(e) => e.target.readOnly = true}></input>
+              <textarea className='input-text' type="text" name="values" ref={calcInput} onFocus={(e) => e.target.readOnly = true}></textarea>
               <div className='result'>{result}</div>
             </div>
             <input className="items clear" value="AC" type="reset" onClick={() => {
